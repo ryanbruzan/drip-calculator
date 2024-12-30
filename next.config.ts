5956e5eb899aacbc6b3930aa360f6386d40ed8de
Ryan Bruzan <ryanbruzan220@gmail.com>
2024-12-30 09:30:34 -0500

    feat: nextjs config for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	basePath: '/drip-calculator',
+	output: 'export',
 };
 
 export default nextConfig;
