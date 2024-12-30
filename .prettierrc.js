0c01ce59bad275a2c9f0fd8f85fd2ee4eec919cb
Ryan Bruzan <ryanbruzan220@gmail.com>
2024-12-30 09:35:37 -0500

    feat: prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	arrowParens: 'always',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	useTabs: true,
+	tabWidth: 4,
+	proseWrap: 'never',
+	quoteProps: 'consistent',
+	singleQuote: true,
+	singleAttributePerLine: true,
+	trailingComma: 'all',
+}
